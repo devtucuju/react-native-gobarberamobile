15248fdccd9cac38d5d5e90246ab692254a813ce
devtucuju <jsdeveloperweb@gmail.com>
2021-02-09 13:52:15 -0300

    #confuguracao navegacao

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { registerRootComponent } from 'expo';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <SafeAreaProvider style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <View />
+        <Routes />
       </SafeAreaProvider>
       <StatusBar style="light" backgroundColor="#312e38" />
-    </>
+    </NavigationContainer>
   );
 };
 
